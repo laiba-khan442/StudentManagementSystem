812f42c1140c3e188e251f48f9a37a5efd5f50e5
Laiba Khan <155344430+laiba-khan442@users.noreply.github.com>
2025-05-04 04:10:46 +0500

    added-constructors-and-methods

--- src/student.cpp
+++ src/student.cpp
@@ -1,8 +1,24 @@
-// This is a student management app student entity cpp file
-#include <iostream> 
+// This is a student management application's method declaration file. 
+#include "student.h"
 using namespace std; 
 
-int main (void){
-    cout << "This is the student file!"; 
-    return 0; 
+//declaration of non-parameterized construtor. 
+student::student (){
+    age = 0;
+    marks = 0.0; 
+    name = "";  
+    roll_num = 0;  
+} 
+
+//declaration of parameterized construtor. 
+student::student (int a, float m, string n, int r){
+    age = a;
+    marks = m; 
+    name = n;  
+    roll_num = r;  
 }
+
+//declaration of getter methods. 
+int student::getAge(){
+    return age; 
+}; 
--- src/student.h
+++ src/student.h
@@ -1,8 +1,35 @@
 //This is a Student Header File
-#include <iostream> 
+#include <iostream>
+#include <string> 
 using namespace std; 
 
-int main (void){
-    cout << "This is the student Header file!"; 
-    return 0; 
-}
+//here we define a class student with it's private attributes such as: age, marks, name and roll number. 
+
+class student{
+    private: 
+        int age; 
+        float marks; 
+        string name; 
+        int roll_num; 
+
+//public methods to access private attributes.  
+    public:
+
+    //non-parameterized constructor defination. 
+        student (); 
+
+    //parameterized constructor defination. 
+       student (int a, float m, string n, int r); 
+
+    //getter methods defination.    
+        int getAge (); 
+        float getMarks (); 
+        string getName (); 
+        int getRollnum (); 
+
+    //Setter methods defination. 
+        int setAge (int a); 
+        float setMarks (int m); 
+        string setName (int n); 
+        int setRollNum (int r); 
+}; 
